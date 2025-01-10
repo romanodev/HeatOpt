b3d99e50ba4b88ee590db7f70673e62e03ea82e6
Giuseppe Romano <romanog@mit.edu>
2025-01-10 13:02:06 -0500

    code simplification

--- heatopt.py
+++ heatopt.py
@@ -24,77 +24,6 @@ warnings.simplefilter('ignore',SparseEfficiencyWarning)
 
 
 
-# def write_stl(x):
-
-#  x = np.array(x)   
-
-#  N = len(x)
-#  Ns = int(np.sqrt(N))
-#  x =x.reshape((Ns,Ns))
-
-#  #Make it periodic--
-#  x = np.pad(x,Ns,mode='wrap')
-
-#  N2 = 3*Ns
-
-#  vertices = []
-#  faces    = []
-
-#  for i, j in product(range(N2+1),range(N2+1)):
-#      vertices.append([i,j,0])
-#  for i, j in product(range(N2+1),range(N2+1)):
-#      vertices.append([i,j,1])
-
-#  def add_pixel(i,j):
-#     v  = [] 
-#     v.append(i*(N2+1)+j)
-#     v.append((i+1)*(N2+1)+j)
-#     v.append((i+1)*(N2+1)+j+1)
-#     v.append(i*(N2+1)+j+1)
-#     v.append(i*(N2+1)+j + (N2+1)*(N2+1))
-#     v.append((i+1)*(N2+1)+j + (N2+1)*(N2+1))
-#     v.append((i+1)*(N2+1)+j+1 + (N2+1)*(N2+1))
-#     v.append(i*(N2+1)+j+1 + (N2+1)*(N2+1))
-   
-
-#     faces.append([v[0],v[3],v[1]])
-#     faces.append([v[1],v[3],v[2]])
-#     faces.append([v[0],v[4],v[7]])
-#     faces.append([v[0],v[7],v[3]])
-#     faces.append([v[4],v[5],v[6]])
-#     faces.append([v[4],v[6],v[7]])
-#     faces.append([v[5],v[1],v[2]])
-#     faces.append([v[5],v[2],v[6]])
-#     faces.append([v[2],v[3],v[6]])
-#     faces.append([v[3],v[7],v[6]])
-#     faces.append([v[0],v[1],v[5]])
-#     faces.append([v[0],v[5],v[4]])
-
-
-#  for i,j in zip(*x.nonzero()):
-#   add_pixel(i,j)    
-
-#  faces    = np.array(faces)
-#  vertices = np.array(vertices)
-
-#  T = 4
-#  vertices[:,2] *=T
-
-
-#  # Create the mesh
-#  cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
-#  for i, f in enumerate(faces):
-#     for j in range(3):
-#         cube.vectors[i][j] = vertices[f[j],:]
-
-#  output = BytesIO()
-#  cube.save('TopOpt',fh=output,mode=Mode.ASCII)
-
-#  # Write the mesh to file "cube.stl"
-#  return output.getvalue().decode("utf-8") 
-
-
-
 
 def create_stl(x):
 
@@ -206,23 +135,6 @@ def get_fig(x,grid,J):
   return fig
 
 
-def compute_kappa_and_gradient(T,rho_dep,direction,N):
-
-      dim = 2
-      [kappa_ij,P_vec,i_mat,j_mat,ii,kappad_ij,kappad_ji] = rho_dep
-      #Compute kappa---
-      kappa     = np.array([np.sum(kappa_ij[ii[i]])*direction[i] - np.dot(T,P_vec[i]) for i in range(dim)])
-
-      gradient = np.zeros(N)
-      np.add.at(gradient,i_mat,kappad_ij*np.power(T[i_mat]-T[j_mat],2))
-      for i in range(dim):
-       tmp      = direction[i]**2 + 2*direction[i]*(T[j_mat[ii[i]]]-T[i_mat[ii[i]]])   
-       np.add.at(gradient,i_mat[ii[i]],kappad_ij[ii[i]]*tmp)
-       np.add.at(gradient,j_mat[ii[i]],kappad_ji[ii[i]]*tmp)
-
-      return np.dot(kappa,direction),gradient
-
-
 def get_grid(N):
 
 
@@ -324,39 +236,6 @@ def get_mapping(grid,R):
    return mapping
 
 
-
-def get_aux(rho,i_mat,j_mat,ind_extremes):
-    
-     k0 = 1e-12;k1 = 1.0
-     kappa_map       = k0 + rho*(k1-k0)
-     kappa_ij     = 2*kappa_map[i_mat] * kappa_map[j_mat]/(kappa_map[i_mat] + kappa_map[j_mat])
-
-     kappad_ij    = (k1-k0)*0.5*np.power(kappa_ij/kappa_map[i_mat],2)
-     kappad_ji    = (k1-k0)*0.5*np.power(kappa_ij/kappa_map[j_mat],2)
-
-     #assembly
-     v1 = np.zeros_like(rho)
-     np.add.at(v1,i_mat,kappa_ij)
-     data = np.hstack((-kappa_ij,v1))
- 
-     N   = len(rho)
-     dim = len(ind_extremes)
-
-     i = np.hstack((i_mat,np.arange(N)))
-     j = np.hstack((j_mat,np.arange(N)))
-
-     P_vec   = np.zeros((dim,N))
-
-     #compute vectorial perturbation
-     for i in range(dim):
-      ii  = ind_extremes[i,1]  
-      np.add.at(P_vec,(i,i_mat[ii]), kappa_ij[ii])
-      np.add.at(P_vec,(i,j_mat[ii]),-kappa_ij[ii])
-
-     return data,i,j,kappa_ij,kappad_ij,kappad_ji,P_vec,kappa_map
-
-
-
 def fourier(**options):
     """Fourier Solver"""
 
@@ -395,11 +274,26 @@ def fourier(**options):
     row = np.hstack((i_mat,np.arange(N))) 
     col = np.hstack((j_mat,np.arange(N))) 
 
-    def func(rho):
-     
-     data,i,j,kappa_ij,kappad_ij,kappad_ji,P_vec,kappa_map = get_aux(rho,i_mat,j_mat,ind_extremes)
+    k0 = 1e-12;k1 = 1.0
 
-     aux = kappa_ij,i_mat,j_mat
+    def func(rho):
+    
+    
+     kappa_map       = k0 + rho*(k1-k0)
+     kappa_ij     = 2*kappa_map[i_mat] * kappa_map[j_mat]/(kappa_map[i_mat] + kappa_map[j_mat])
+
+     kappad_ij    = (k1-k0)*0.5*np.power(kappa_ij/kappa_map[i_mat],2)
+     kappad_ji    = (k1-k0)*0.5*np.power(kappa_ij/kappa_map[j_mat],2)
+
+
+     #compute vectorial perturbation
+     P_vec   = np.zeros((dim,N))
+     for i in range(dim):
+      r  = ii[i] 
+      np.add.at(P_vec,(i,i_mat[r]), kappa_ij[r])
+      np.add.at(P_vec,(i,j_mat[r]),-kappa_ij[r])   
+
+
 
      #Assemble matrix
      tmp = np.zeros_like(rho)
@@ -411,35 +305,37 @@ def fourier(**options):
      #---------------------
 
      #---------------------------------------
-     kappa     = np.zeros(n_dir)
-     jacobian  = np.zeros((n_dir,N))
+    
     
 
      P =  np.einsum('di,ic->cd',directions,P_vec)   
      P[12,:] = 0 
      T_mat  = lu.solve(P)
-     #print(T_mat.shape)
-     #quit()
+   
 
-     rho_dep = [kappa_ij,P_vec,i_mat,j_mat,ii,kappad_ij,kappad_ji]
+     jacobian = np.zeros((n_dir,N))
+     np.add.at(jacobian.T,i_mat,np.einsum('s,sd->sd',kappad_ij,np.power(T_mat[i_mat]-T_mat[j_mat],2)))
+     tmp = (directions.T**2)[:,None,:] + 2*directions.T[:,None,:]*(T_mat[j_mat[ii]]-T_mat[i_mat[ii]])
+     np.add.at(jacobian.T,i_mat[ii],kappad_ij[ii][:,:,None]*tmp)
+     np.add.at(jacobian.T,j_mat[ii],kappad_ji[ii][:,:,None]*tmp)
 
-     #This needs to be vectorized
     
-     for n,direction in enumerate(directions):
-      kappa_and_gradient =  partial(compute_kappa_and_gradient,rho_dep=rho_dep,N=N,direction=direction)
-      kappa[n],jacobian[n] = kappa_and_gradient(T_mat[:,n])
+     kappa = np.einsum('is,di->d',kappa_ij[ii],directions**2) -  np.einsum('cd,ic,di->d',T_mat,P_vec,directions) 
 
      
      kappa    = np.matmul(Ainv,kappa)
      #print(kappa)
      jacobian = np.einsum('ji,ik->jk',Ainv,jacobian)
 
+
      return kappa,jacobian,T_mat
 
     
     def compute_flux(rho,T_mat):
       
-     data,i,j,kappa_ij,kappad_ij,kappad_ji,P_vec,kappa_map = get_aux(rho,i_mat,j_mat,ind_extremes)
+
+     kappa_map       = k0 + rho*(k1-k0)
+     kappa_ij     = 2*kappa_map[i_mat] * kappa_map[j_mat]/(kappa_map[i_mat] + kappa_map[j_mat])
 
      J = np.zeros((n_dir,2,N))
 
@@ -459,14 +355,14 @@ def fourier(**options):
       #Periodic
 
       for i in range(dim):
-       ii  = ind_extremes[i,1]
+       r = ii[i]
 
-       contribution = direction[i]*np.einsum('s,sd->sd',kappa_ij[ii],normals[ii])
+       contribution = direction[i]*np.einsum('s,sd->sd',kappa_ij[r],normals[r])
       
-       np.add.at(J[n,0],i_mat[ii],-contribution[:,0])
-       np.add.at(J[n,1],i_mat[ii],-contribution[:,1])
-       np.add.at(J[n,0],j_mat[ii],-contribution[:,0])
-       np.add.at(J[n,1],j_mat[ii],-contribution[:,1])
+       np.add.at(J[n,0],i_mat[r],-contribution[:,0])
+       np.add.at(J[n,1],i_mat[r],-contribution[:,1])
+       np.add.at(J[n,0],j_mat[r],-contribution[:,0])
+       np.add.at(J[n,1],j_mat[r],-contribution[:,1])
 
 
 
@@ -520,6 +416,7 @@ def get_optimizer():
   opt = nlopt.opt(nlopt.LD_CCSAQ,N)
 
   x = np.random.rand(N)
+  
   #x.dump('x')
   #x = np.load('x',allow_pickle=True)
   #---------------------
@@ -563,8 +460,9 @@ def get_optimizer():
 if __name__ =='__main__':
 
  optimizer,grid= get_optimizer()
-
+ a = time.time()
  kappa,fig,x,J = optimizer(0.2,0.2,-0.05)
+ print(time.time()-a)
 
  #Save the json file
  print(kappa)
